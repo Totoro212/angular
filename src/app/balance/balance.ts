5c91a622ebfdf4f24d7e475379d8ca289eaa111f
name <email@gmail.com>
2026-02-20 17:07:37 +0500

    add viewer history

--- src/app/balance/balance.ts
+++ src/app/balance/balance.ts
@@ -2,6 +2,7 @@ import { Component, computed, inject } from '@angular/core';
 import { AccountsService } from '../services/accounts-service';
 import { AuthService } from '../services/auth-service';
 import { AccOpeationService } from '../services/acc-opeation-service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-balance',
   imports: [],
@@ -10,13 +11,17 @@ import { AccOpeationService } from '../services/acc-opeation-service';
 })
 export class Balance {
   accountService = inject(AccountsService)
+  router = inject(Router)
   authService = inject(AuthService)
   accOperationsService =inject(AccOpeationService)
-  
+
   accounts = this.accountService.getAllAccounts()
   currentUser = computed(()=>this.accountService.getUserByLogin(this.authService.currentLogin()))
 
   deleteAccount(login:string){
     this.accOperationsService.deleteAccount(login)
+  }
+  openHistory(login:string){
+    this.router.navigate(['history', login])
   }
 }
--- src/app/history-operations/history-operations.ts
+++ src/app/history-operations/history-operations.ts
@@ -1,9 +1,10 @@
-import { Component, inject, computed } from '@angular/core';
+import { Component, inject, computed, signal } from '@angular/core';
 import { OperationsService } from '../services/operations-service';
 import { AccountsService } from '../services/accounts-service';
 import { DatePipe } from '@angular/common';
 import { AuthService } from '../services/auth-service';
 import { AccOpeationService } from '../services/acc-opeation-service';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-history-operations',
   imports: [DatePipe],
@@ -11,14 +12,20 @@ import { AccOpeationService } from '../services/acc-opeation-service';
   styleUrl: './history-operations.css',
 })
 export class HistoryOperations {
+  route = inject(ActivatedRoute)
   authService = inject(AuthService)
   operationsService = inject(OperationsService)
   accOpeationService = inject(AccOpeationService)
   accountsService = inject(AccountsService)
   
   operations = this.operationsService.getAllOperations()
-  currentUser = computed(()=>this.accountsService.getUserByLogin(this.authService.currentLogin()))
-  
+  loginUser = signal('')
+  currentUser = computed(()=>this.accountsService.getUserByLogin(this.loginUser()))
+  constructor(){
+    this.route.params.subscribe((params)=>{
+      this.loginUser.set(params['login'])
+    })
+  }
   deleteOperation(id:number, operation:boolean, sum:number, login:string){
     this.accOpeationService.deleteOperation(id, operation, sum, login)
   }
