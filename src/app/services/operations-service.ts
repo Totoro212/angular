95dd6cd675d31b94e7fa02950e4732b7b98a9daf
name <email@gmail.com>
2026-02-21 12:41:03 +0500

    Revert "change id"
    This reverts commit 1c6981552e24095d28e801d748e0e2fa57ce3e90.

--- src/app/services/operations-service.ts
+++ src/app/services/operations-service.ts
@@ -40,7 +40,7 @@ export class OperationsService {
 
   makeTransaction(login:string, sum:number, operation:boolean){
     const newOperation:OperationInterface = {
-      id: Date.now(),
+      id: this.operations().length+1,
       login,
       sum,
       operation,
